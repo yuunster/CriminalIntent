e00693a1ac9a4b02da605d28e8b8cef02fb6067c
Justin <kayuuha@gmail.com>
2023-09-25 21:59:30 -0700

    Chapter 14: Implemented a date dialog wrapped in a fragment when a user clicks on the date button.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeDetailBinding
 import kotlinx.coroutines.launch
@@ -50,6 +52,11 @@ class CrimeDetailFragment : Fragment() {
                 crimeTitle.setText(crime.title)
             }
             crimeDate.text = crime.date.toString()
+            crimeDate.setOnClickListener {
+                findNavController().navigate(
+                    CrimeDetailFragmentDirections.selectDate(crime.date)
+                )
+            }
             crimeSolved.isChecked = crime.isSolved
         }
     }
@@ -61,10 +68,6 @@ class CrimeDetailFragment : Fragment() {
                 crimeDetailViewModel.updateCrime { oldCrime ->
                     oldCrime.copy(title = text.toString())
                 }
-            }
-
-            crimeDate.apply {
-                isEnabled = false
             }
 
             crimeSolved.setOnCheckedChangeListener { _, isChecked, ->
@@ -80,6 +83,14 @@ class CrimeDetailFragment : Fragment() {
                     crime?.let { updateUi(it) }
                 }
             }
+        }
+
+        setFragmentResultListener(
+            DatePickerFragment.REQUEST_KEY_DATE
+        ) { requestKey, bundle ->
+            val newDate =
+                bundle.getSerializable(DatePickerFragment.BUNDLE_KEY_DATE) as Date
+            crimeDetailViewModel.updateCrime { it.copy(date = newDate) }
         }
     }
 }
